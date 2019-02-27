80ffdbe7ade2e02167cd07a5c852d833055a9f98
Nathaniel Johnson <njj878@mun.ca>
2019-02-27 00:33:37 -0330

    Error messages are shown with improper email or password.

--- app/src/main/java/ca/outercove/uomiapplication/LoginRegistrationActivity.java
+++ app/src/main/java/ca/outercove/uomiapplication/LoginRegistrationActivity.java
@@ -11,9 +11,9 @@ import android.widget.EditText;
 public class LoginRegistrationActivity extends AppCompatActivity {
 
     // Fields of the activity
-    private EditText mEmailAddress;
-    private EditText mPassword;
-    private Button mBtnSignInRegister;
+    protected EditText mEmailAddress;
+    protected EditText mPassword;
+    protected Button mBtnSignInRegister;
 
     //LoginVerification object
     private LoginVerification loginVerification;
@@ -34,10 +34,17 @@ public class LoginRegistrationActivity extends AppCompatActivity {
         mBtnSignInRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (loginVerification.loginValidation(getEmail(), getPassword())) {
+                if(!loginVerification.validateEmail(getEmail())){
+                    mEmailAddress.setError("Invalid e-mail");
+                }
+                if (!loginVerification.validatePassword(getPassword())) {
+                    mPassword.setError("Must be alphanumeric with 6-20 characters");
+                }
+                if (loginVerification.loginValidation(getEmail(), getPassword())){
                     Intent successfulLogin = new Intent(LoginRegistrationActivity.this, MainActivity.class);
                     startActivity(successfulLogin);
                 }
+
                 // TODO: show a message when one of the fields is invalid
 
             }
