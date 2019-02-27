3cc05b4617e8d6b858d083b223afaca021d013dc
Matt Hickey <mjh060@mun.ca>
2019-02-27 15:18:50 -0330

    deleting useless line

--- app/src/main/java/ca/outercove/uomiapplication/listClasses/AccountsViewContent.java
+++ app/src/main/java/ca/outercove/uomiapplication/listClasses/AccountsViewContent.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 
 import ca.outercove.uomiapplication.appObjects.User;
-import ca.outercove.uomiapplication.dummy.DummyContent;
 
 public class AccountsViewContent {
 
