92b57f6c4919cc497b3ef703b067f6770bfc9f31
Matt Hickey <mjh060@mun.ca>
2019-02-28 22:42:05 -0330

    still tweaking appearance

--- app/src/main/java/ca/outercove/uomiapplication/fragments/CreateAccountDialogFragment.java
+++ app/src/main/java/ca/outercove/uomiapplication/fragments/CreateAccountDialogFragment.java
@@ -23,7 +23,7 @@ public class CreateAccountDialogFragment extends DialogFragment {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         // TODO: Create the account
-                        CreateAccountDialogFragment.this.getDialog().cancel();
+                        throw new UnsupportedOperationException("Create account not implemented");
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
